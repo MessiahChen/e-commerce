6debc742151686fee0dd8297c8da4f19d71a5cec
863253972@qq.com <863253972@qq.com>
2020-07-02 14:33:47 +0800

    利用order模块跑通redis

---
+++ e-commerce-order/src/main/java/com/ecommerce/service/impl/TestRedisServiceImpl.java
@@ -0,0 +1,63 @@
+package com.ecommerce.service.impl;
+
+import com.ecommerce.service.RedisService;
+import com.ecommerce.service.TestRedisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * 会员管理Service
+ * Created by macro on 2018/8/3.
+ */
+@Service
+public class TestRedisServiceImpl implements TestRedisService {
+    @Autowired
+    private RedisService redisService;
+
+    @Override
+    public String generateAuthCode(String telephone) {
+        redisService.set("test", telephone);
+        return (String) redisService.get("test");
+    }
+
+    @Override
+    public String verifyAuthCode(String telephone, String authCode) {
+        return null;
+    }
+
+
+    //    @Value("${redis.key.prefix.authCode}")
+//    private String REDIS_KEY_PREFIX_AUTH_CODE;
+//    @Value("${redis.key.expire.authCode}")
+//    private Long AUTH_CODE_EXPIRE_SECONDS;
+//
+//    @Override
+//    public String generateAuthCode(String telephone) {
+//        StringBuilder sb = new StringBuilder();
+//        Random random = new Random();
+//        for (int i = 0; i < 6; i++) {
+//            sb.append(random.nextInt(10));
+//        }
+//        //验证码绑定手机号并存储到redis
+//        redisService.set(REDIS_KEY_PREFIX_AUTH_CODE + telephone, sb.toString());
+//        redisService.expire(REDIS_KEY_PREFIX_AUTH_CODE + telephone, AUTH_CODE_EXPIRE_SECONDS);
+//        return "获取验证码成功";
+//    }
+//
+//
+//    //对输入的验证码进行校验
+//    @Override
+//    public String verifyAuthCode(String telephone, String authCode) {
+//        if (StringUtils.isEmpty(authCode)) {
+//            return "请输入验证码";
+//        }
+//        String realAuthCode = redisService.get(REDIS_KEY_PREFIX_AUTH_CODE + telephone);
+//        boolean result = authCode.equals(realAuthCode);
+//        if (result) {
+//            return "验证码校验成功";
+//        } else {
+//            return "验证码不正确";
+//        }
+//    }
+
+}
