ea630b0dd3e8ca4747b84d8bd49e9994dd851844
qiuxiaolin123 <260028891@qq.com>
2020-07-07 17:44:03 +0800

    complete bvo order module

--- e-commerce-order/src/main/java/com/ecommerce/controller/BvoOrderController.java
+++ e-commerce-order/src/main/java/com/ecommerce/controller/BvoOrderController.java
@@ -1,8 +1,18 @@
 package com.ecommerce.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ecommerce.common.base.BaseController;
+import com.ecommerce.common.base.CommonResult;
+import com.ecommerce.common.exception.BusinessException;
+import com.ecommerce.pojo.SalSalesOrderLineItem;
+import com.ecommerce.pojo.StoStoreOrder;
+import com.ecommerce.pojo.StrStore;
+import com.ecommerce.service.BvoOrderService;
+import com.ecommerce.vo.SaoSalesOrderVO;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @ProjectName: e-commerce
@@ -16,6 +26,51 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 @RestController
 @RequestMapping("/bvoOrder")
-public class BvoOrderController {
-    
+public class BvoOrderController extends BaseController {
+    @Autowired
+    private BvoOrderService bvoOrderService;
+
+    @ApiOperation("根据dsrId获取订单信息->dsr：借卖方对象")
+    @GetMapping("/getVosByDsrId")
+    public CommonResult<List<SaoSalesOrderVO>> getVo(@RequestParam int dsrId){
+        List<SaoSalesOrderVO> saoSalesOrderVOS = bvoOrderService.getSaoVosByDsrId(dsrId);
+        if(saoSalesOrderVOS == null){
+            throw BusinessException.SELECT_FAIL;
+        }else {
+            return CommonResult.success(saoSalesOrderVOS,"返回数据成功");
+        }
+    }
+
+    @ApiOperation("根据saoId获取sal对象")
+    @GetMapping("/getSalBySaoId")
+    public CommonResult<SalSalesOrderLineItem> getSalBySaoId(int saoId){
+        SalSalesOrderLineItem sal = bvoOrderService.getSalBySaoId(saoId);
+        if(sal == null){
+            throw BusinessException.SELECT_FAIL;
+        }else {
+            return CommonResult.success(sal,"返回数据成功");
+        }
+    }
+
+    @ApiOperation("支付以后，根据saoId修改对应的订单状态")
+    @GetMapping("/update")
+    public CommonResult<Integer> update(int saoId){
+        int result = bvoOrderService.updateOrderBySaoId(saoId);
+        if(result == 0){
+            throw BusinessException.UPDATE_FAIL;
+        }else {
+            return CommonResult.success(result,"返回数据成功");
+        }
+    }
+
+    @ApiOperation("根据saoId获取运费")
+    @GetMapping("/getFeeBySaoId")
+    public CommonResult<Double> getFeeBySaoId(int saoId){
+        double fee = bvoOrderService.getFreightFeeBySaoId(saoId);
+        if(fee == -1){
+            throw BusinessException.SELECT_FAIL;
+        }else {
+            return CommonResult.success(fee,"返回数据成功");
+        }
+    }
 }
--- e-commerce-order/src/main/java/com/ecommerce/controller/OrderController.java
+++ e-commerce-order/src/main/java/com/ecommerce/controller/OrderController.java
@@ -21,41 +21,6 @@ public class OrderController extends BaseController {
     @Autowired
     private OrderService orderService;
 
-    /**
-     * sto->原式订单信息
-     * sol->原式订单详细信息
-     * sao->销售订单
-     * sal->销售订单明细
-     */
-//    @ApiOperation("返回所有的sto对象,sto->原式订单信息")
-//    @GetMapping("/sto")
-//    public CommonResult<List<StoStoreOrder>> getStoStoreOrder(){
-//        if(orderService.getAllStoStoreOrder() != null)
-//            return CommonResult.success(orderService.getAllStoStoreOrder(),"获取sto数据成功");
-//        else
-//            throw BusinessException.SELECT_FAIL;
-//    }
-//
-//    @ApiOperation("根据状态返回sol对象,sol->原式订单详细信息")
-//    @GetMapping("/sol")
-//    public CommonResult<List<SolStoreOrderLineItem>>  getSolStoreOrderLineItem(@RequestParam String stsCd){
-//        if(orderService.getSolStoreOrderLineItem(stsCd) != null){
-//            return CommonResult.success("获取sol数据成功");
-//        }else {
-//            throw BusinessException.SELECT_FAIL;
-//        }
-//    }
-//
-//    @ApiOperation("根据状态sal对象,sal->销售订单详细信息")
-//    @GetMapping("/sal")
-//    public CommonResult<List<SalSalesOrderLineItem>>  getSalSalesOrderLineItem(@RequestParam String stsCd){
-//        if(orderService.getSalSalesOrderLineItem(stsCd) != null){
-//            return CommonResult.success("获取sal数据成功");
-//        }else {
-//            throw BusinessException.SELECT_FAIL;
-//        }
-//    }
-
     @ApiOperation("根据manId获得saoVo对象列表，sao->销售订单")
     @GetMapping("/sao")
     public CommonResult<List<SaoSalesOrderVO>> getSaoByManId(@RequestParam int manId){
