a677091d53a0e1a78dfa3cde2abc94287ea1b8c7
Yousabla <907078284@qq.com>
2020-07-08 11:26:05 +0800

    update e-commerce-wallet/src/main/java/com/ecommerce/service/impl/WalletServiceImpl.java.

--- e-commerce-wallet/src/main/java/com/ecommerce/service/impl/WalletServiceImpl.java
+++ e-commerce-wallet/src/main/java/com/ecommerce/service/impl/WalletServiceImpl.java
@@ -38,7 +38,7 @@ public class WalletServiceImpl implements WalletService {
         WaaWalletAccountExample example = new WaaWalletAccountExample();
         example.createCriteria().andAccountNameEqualTo(walletAccountVO.getAccountName());
         List<WaaWalletAccount> accounts = waaWalletAccountMapper.selectByExample(example);
-        if (accounts.get(0) != null){
+        if (accounts != null){
             throw BusinessException.USERNAME_DUPLICATE;
         }
 
