41d2332159243cd00ebdbc36dac8e20f3cf6ddc1
863253972@qq.com <863253972@qq.com>
2020-07-08 17:32:59 +0800

    马洪升2020年7月8日17:32:58

--- e-commerce-product/src/main/java/com/ecommerce/service/impl/ProductBrowseServiceImpl.java
+++ e-commerce-product/src/main/java/com/ecommerce/service/impl/ProductBrowseServiceImpl.java
@@ -1,15 +1,22 @@
 package com.ecommerce.service.impl;
 
+import com.ecommerce.dao.CatCategoryMapper;
 import com.ecommerce.dao.ImgImageMapper;
 import com.ecommerce.dao.PrcProductCategoryMapper;
 import com.ecommerce.dao.ProProductMapper;
+import com.ecommerce.dto.product.browse.ProductBrowseDTO;
+import com.ecommerce.pojo.CatCategory;
+import com.ecommerce.pojo.CatCategoryExample;
 import com.ecommerce.pojo.ProProduct;
 import com.ecommerce.pojo.ProProductExample;
 import com.ecommerce.service.ProductBrowseService;
 import com.ecommerce.vojo.browse.ProductBrowseWithCatVO;
+import com.ecommerce.vojo.image.ProductCategoryVO;
+import com.ecommerce.vojo.image.ViceCategory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,20 +31,30 @@ public class ProductBrowseServiceImpl implements ProductBrowseService {
     @Autowired
     private ImgImageMapper imgImageMapper;
 
+    @Autowired
+    private CatCategoryMapper catCategoryMapper;
+
     @Override
     public List<ProductBrowseWithCatVO> getAllProductWithStatD() {
-        ProProductExample proProductExample = new ProProductExample();
-        ProProductExample.Criteria criteria_pro = proProductExample.createCriteria();
-        criteria_pro.andStsCdEqualTo("c");
+        List<ProductBrowseWithCatVO> result = new ArrayList<>();
+        // 将所有主分类取出来
+        // 去prc中找到不同主分类对应的pro_id
+        // 根据pro_id找到pro
+        CatCategoryExample catCategoryExample = new CatCategoryExample();
+        CatCategoryExample.Criteria criteria_cat = catCategoryExample.createCriteria();
+        criteria_cat.andCatFatherIdIsNull();
 
-        List<ProProduct> proProducts = proProductMapper.selectByExample(proProductExample);
+        List<CatCategory> mainCatCategories = catCategoryMapper.selectByExample(catCategoryExample);
 
-//        for (ProProduct pro : proProducts) {
-//
-//
-//            prcProductCategoryMapper.selectByExample();
-//        }
+        for (CatCategory cat : mainCatCategories) {
+            ProductBrowseWithCatVO productBrowseWithCatVO = new ProductBrowseWithCatVO();
+            productBrowseWithCatVO.setCatName(cat.getCatName());
+            List<ProductBrowseDTO> productBrowseDTOS = proProductMapper.selectProByPrcCat(cat.getCatId());
+            productBrowseWithCatVO.setProducts(productBrowseDTOS);
 
-        return null;
+            result.add(productBrowseWithCatVO);
+        }
+
+        return result;
     }
 }
